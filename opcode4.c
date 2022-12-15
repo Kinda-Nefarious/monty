25a81dd51f301ec2ae01dbf03bbffce97af2cbfc
Rufaro Nyakudya <47318943+Kinda-Nefarious@users.noreply.github.com>
2022-12-15 12:05:11 +0200

    Corrections

--- opcode4.c
+++ opcode4.c
@@ -140,24 +140,3 @@ void delete_dnodeint_at_index(stack_t **head, unsigned int index)
 		free(current);
 	}
 }
-/**
- * _stack - Converts a queue to a stack.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- */
-void _stack(stack_t **stack, unsigned int line_number)
-{
-	(*stack)->n = STACK;
-	(void)line_number;
-}
-
-/**
- * _queue - Converts a stack to a queue.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- */
-void _queue(stack_t **stack, unsigned int line_number)
-{
-	(*stack)->n = QUEUE;
-	(void)line_number;
-}
